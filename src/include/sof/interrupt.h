63e3c1fda2c87d3367ce1816404767adbb069f93
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2019-03-26 14:20:42 +0100

    interrupt: add doxygen documentation for struct irq_desc
    Other global interrupt-related structs are already documented, add
    missing documentation to struct irq_desc
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- src/include/sof/interrupt.h
+++ src/include/sof/interrupt.h
@@ -35,18 +35,23 @@ struct irq_child {
 	struct list_item list;
 };
 
+/**
+ * struct irq_desc - interrupt client descriptor
+ *
+ * @irq:	virtual IRQ number
+ * @handler:	interrupt handler function
+ * @handler_arg: interrupt handler argument
+ * @unmask:	whether irq should be automatically unmasked
+ * @cpu_mask:	a mask of CPUs on which this interrupt is enabled
+ * @irq_list:	to link to other irq_desc
+ */
 struct irq_desc {
-	int irq;        /* logical IRQ number */
-
+	int irq;
 	void (*handler)(void *arg);
 	void *handler_arg;
-
-	/* whether irq should be automatically unmasked */
 	int unmask;
-
 	uint32_t cpu_mask;
 
-	/* to link to other irq_desc */
 	struct list_item irq_list;
 };
 
